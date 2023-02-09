53cea984c9d76a30af7f4ff7128df09db3b30fad
mohammadrad81 <mohammad.rad81@gmail.com>
2023-02-09 17:17:49 +0330

    work with machine learning model views implemented

---
+++ machine_learning/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import FitView, FitTransformView, TransformView, PredictView, PredictProbaView
+
+urlpatterns = [
+    path('api/fit/<int:pk>/', FitView.as_view(), name='fit'),
+    path('api/transform/<int:pk>/', TransformView.as_view(), name='transform'),
+    path('api/fit-transform/<int:pk>/', FitTransformView.as_view(), name='fit-transform'),
+    path('api/predict/<int:pk>/', PredictView.as_view(), name='predict'),
+    path('api/predict-proba/<int:pk>/', PredictProbaView.as_view(), name='predict-proba'),
+]
--- machine_learning/views.py
+++ machine_learning/views.py
@@ -1,3 +1,62 @@
 from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import MachineLearningModel
 
-# Create your views here.
+
+class FitView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, pk):
+        data = request.data
+        x = data['x']
+        y = data.get('y', None)
+        machine_learning_model = MachineLearningModel.objects.get(pk=pk)
+        machine_learning_model.fit(x, y)
+        return Response({"message": f"model with id {pk} fitted with data"}, status=status.HTTP_200_OK)
+
+
+class TransformView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, pk):
+        data = request.data
+        x = data['x']
+        machine_learning_model = MachineLearningModel.objects.get(pk=pk)
+        transformed_data = machine_learning_model.transform(x)
+        return Response({'result': transformed_data}, status=status.HTTP_200_OK)
+
+
+class FitTransformView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, pk):
+        data = request.data
+        x = data['x']
+        machine_learning_model = MachineLearningModel.objects.get(pk=pk)
+        transformed_data = machine_learning_model.fit_transform(x)
+        return Response({'result': transformed_data}, status=status.HTTP_200_OK)
+
+
+class PredictView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, pk):
+        data = request.data
+        x = data['x']
+        machine_learning_model = MachineLearningModel.objects.get(pk=pk)
+        transformed_data = machine_learning_model.predict(x)
+        return Response({'result': transformed_data}, status=status.HTTP_200_OK)
+
+
+class PredictProbaView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, pk):
+        data = request.data
+        x = data['x']
+        machine_learning_model = MachineLearningModel.objects.get(pk=pk)
+        transformed_data = machine_learning_model.predict_proba(x)
+        return Response({'result': transformed_data}, status=status.HTTP_200_OK)
