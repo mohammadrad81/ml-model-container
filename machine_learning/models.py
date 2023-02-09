6a04f56e661ed6d4c8539c7cc24eedad1a509a63
mohammadrad81 <mohammad.rad81@gmail.com>
2023-02-09 16:55:31 +0330

    MachineLearningModel

--- machine_learning/models.py
+++ machine_learning/models.py
@@ -1,4 +1,44 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+import numpy as np
+import pickle
+from sklearn.tree import DecisionTreeClassifier
 
 
-# Create your models here.
+class MachineLearningModel(models.Model):
+    name = models.CharField(max_length=200, blank=True, default='')
+    description = models.TextField(blank=True, default='')
+    file = models.FileField(null=False)
+
+    def load(self):
+        ml_model = pickle.load(self.file)
+        return ml_model
+
+    def fit(self, x, y=None):
+        x = np.array(x)
+        ml_model = self.load()
+        if y == None:
+            ml_model.fit(x)
+        else:
+            y = np.array(y)
+            ml_model.fit(x, y)
+
+    def transform(self, x):
+        x = np.array(x)
+        ml_model = self.load()
+        return ml_model.transform(x).tolist()
+
+    def fit_transform(self, x):
+        x = np.array(x)
+        ml_model = self.load()
+        return ml_model.fit_transform(x).tolist()
+
+    def predict(self, x):
+        x = np.array(x)
+        ml_model = self.load()
+        return ml_model.predict(x).tolist()
+
+    def predict_proba(self, x):
+        x = np.array(x)
+        ml_model = self.load()
+        return ml_model.predict_proba(x).tolist()
